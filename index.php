90a737c0b3800c0cbb2976c8b2a7e5382e614204
Khanh <giakhanh010200@gmail.com>
2023-08-18 18:35:41 +0700

    commit

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $html = file_get_html('https://dantri.com.vn/giao-duc/vu-lo-de-thi-sinh-8-thi-si
 // echo $html;
 //lấy phần đọc nhiều
 
-echo "ĐỌC NHIỀU";
+echo "<h1>".$html->find(".article-lot .article-head",0)."</h1>";
 echo '<br>';
-for($i = 0; $i < 3; $i ++){
+for($i = 1; $i < 4; $i++){
     $lot = $html->find(".article-lot .article-item",$i);
     $thumb = $lot->find(".article-thumb img",0)->getAttribute("data-src");
     $title = $lot->find(".article-title a",0)->text();
@@ -19,28 +19,33 @@ for($i = 0; $i < 3; $i ++){
     // $thumb = $lot->find(".article-title");
     // echo $lot;
 }
-echo "TIN LIÊN QUAN";
 echo '<br>';
-for($i = 0; $i < 3; $i ++){
+echo '<br>';
+echo "<h1>".$html->find(".article-related .title-head",0)."</h1>";
+echo '<br>';
+for($i = 0; $i < 3; $i++){
     $related = $html->find('.article-related .article-item',$i);
     // $thumb = $lot->find(".article-title");
     $thumb = $related->find(".article-thumb img",0)->getAttribute("data-src");
     $title = $related->find(".article-title a",0)->text();
-    $description = $related->find(".article-excerpt",0)->text;
+    $description = $related->find(".article-excerpt a",0)->text();
     $url = $related->find(".article-title a",0)->getAttribute("href");
     echo "<img src ='".$thumb."'>";
     echo '<br>';
     $url = "https://dantri.com.vn".$url;
     echo "<a href='".$url."' target='_blank'>".$title."</a>";
     echo '<br>';
-    echo "<p>".$description."</p>";
+    echo $description;
 }
-echo "ĐANG ĐƯỢC QUAN TÂM";
-for($i = 0; $i < 3; $i ++){
-    $care = $html->find('.article-care .article-item',$i);
-    // $thumb = $lot->find(".article-title");
-    $thumb = $care->find(".article-thumb img",0)->getAttribute("data-src");
+echo '<br>';
+echo '<br>';
+echo "<h1>".$html->find(".article-care.list .article-head",0)."</h1>";
+echo '<br>';
+for($i = 0; $i < 3; $i++){
+    $care = $html->find(".article-care .article-item",$i);
+    $thumb = $care->find(".article-thumb img",0)->getAttribute("src");
     $title = $care->find(".article-title a",0)->text();
+    $description = $care->find(".article-excerpt a",0)->text();
     $url = $care->find(".article-title a",0)->getAttribute("href");
     echo "<img src ='".$thumb."'>";
     echo '<br>';
@@ -49,12 +54,4 @@ for($i = 0; $i < 3; $i ++){
     echo '<br>';
     echo "<p>".$description."</p>";
 }
-// $html ->load($html ->save());
-//     $tieude = $html->find('.article-thumb');
-//     $noidung = $html->find('article_title');
-
-// $html = $html->find('.article-lot.line',0);
-// var_dump($html);
-
-
 ?>
